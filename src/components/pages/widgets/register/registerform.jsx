7636185d52b6ee7827cc1cfbb03062ab0b9db65b
ctrl-beepboop <ctrl-beepboop@ok.de>
2021-04-28 19:51:03 +0200

    some small adjustment

--- src/components/pages/widgets/register/registerform.jsx
+++ src/components/pages/widgets/register/registerform.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../../../common/forms";
 import Joi from "joi";
 
 import userService from "../../../../services/userService";
 
-class RegisterForm extends Form {
-    state = {
-        data: { username: "", email: "", password: "", repassword: "" },
-        errors: {},
-    };
+const RegisterForm = () => {
+    const [username, setUsername] = useState(0);
+    const [email, setEmail] = useState(0);
+    const [password, setPassword] = useState(0);
+    const [repassword, setRepassword] = useState(0);
 
-    schema = Joi.object({
+    const schema = Joi.object({
         username: Joi.string().min(5).required().messages({
             "string.empty": "Field Username is required!",
             "string.min": "Your username must have atleast 5 characters!",
@@ -25,37 +25,71 @@ class RegisterForm extends Form {
         repassword: Joi.ref("password"),
     });
 
-    // Everything success!
-    submit = () => {
-        userService.registerUser(this.state.data).then((response) => {
-            if (!response.status === 200) {
-                console.log("user registered");
-                return true;
-            }
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-            this.setState({
-                errors: "Ein unerwarteter Fehler ist aufgetreten",
-            });
-            console.log(this.state);
-        });
+        console.log(schema.validate({ username }));
     };
 
-    render() {
-        return (
-            <React.Fragment>
-                <form
-                    onSubmit={this.handleSubmit}
-                    className="register-formular"
-                >
-                    {this.renderInput("username", "Username")} <br />
-                    {this.renderInput("email", "E-Mail Adress")} <br />
-                    {this.renderInput("password", "Password")} <br />
-                    {this.renderInput("repassword", "Re-Password")} <br />
-                    {this.renderSubmitButton("submitRegister")} <br />
-                </form>
-            </React.Fragment>
-        );
-    }
-}
+    const handleFormularChange = (e) => {
+        e.preventDefault();
+
+        const formularName = e.target.name;
+        const formularValue = e.target.value;
+
+        switch (formularName) {
+            case "username":
+                setUsername(formularValue);
+                break;
+
+            case "email":
+                setEmail(formularValue);
+                break;
+
+            case "password":
+                setPassword(formularValue);
+                break;
+
+            case "repassword":
+                setRepassword(formularValue);
+                break;
+        }
+    };
+
+    return (
+        <React.Fragment>
+            <form
+                method="post"
+                onSubmit={handleSubmit}
+                onChange={handleFormularChange}
+            >
+                <label htmlFor="username">
+                    <p>Username</p>
+                    <input type="text" name="username" />
+                </label>
+                <br />
+                <br />
+                <label htmlFor="email">
+                    <p>E-Mail</p>
+                    <input type="text" name="email" />
+                </label>
+                <br />
+                <br />
+                <label htmlFor="password">
+                    <p>Password</p>
+                    <input type="text" name="password" />
+                </label>
+                <br />
+                <br />
+                <label htmlFor="repassword">
+                    <p>Re-Password</p>
+                    <input type="text" name="repassword" />
+                </label>
+
+                <input type="submit" />
+            </form>
+        </React.Fragment>
+    );
+};
 
 export default RegisterForm;
