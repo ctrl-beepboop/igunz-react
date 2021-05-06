0427cf30e0ae36eb0307f24da4549501365842c9
ctrl-beepboop <wieland.f.wueller@gmail.com>
2021-05-06 19:56:42 +0200

    rebase

--- src/components/pages/widgets/login/loginform.jsx
+++ src/components/pages/widgets/login/loginform.jsx
@@ -1,40 +1,7 @@
 import React from "react";
-import Form from "../../../common/forms";
 
-import Joi from "joi";
-
-class LoginForm extends Form {
-    state = {
-        data: { username: "", password: "" },
-        errors: {},
-    };
-
-    schema = Joi.object({
-        username: Joi.string().min(5).required().messages({
-            "string.empty": "Field Username is required!",
-            "string.min": "Your username must have atleast 5 characters!",
-        }),
-        password: Joi.string().min(5).required().messages({
-            "string.empty": "Field Password is required!",
-            "string.min": "Your Password must have atleast 5 characters!",
-        }),
-    });
-
-    submit = () => {
-        console.log("gut ja");
-    };
-
-    render() {
-        return (
-            <React.Fragment>
-                <form onSubmit={this.handleSubmit} className="login-formular">
-                    {this.renderInput("username", "Username")} <br />
-                    {this.renderInput("password", "Password")} <br />
-                    {this.renderSubmitButton("submitLogin")} <br />
-                </form>
-            </React.Fragment>
-        );
-    }
-}
+const LoginForm = () => {
+    return <h1>LoginForm</h1>;
+};
 
 export default LoginForm;
--- src/components/pages/widgets/register/registerform.jsx
+++ src/components/pages/widgets/register/registerform.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Form from "../../../common/forms";
 import Joi from "joi";
-
-import userService from "../../../../services/userService";
 
 const RegisterForm = () => {
     const schema = Joi.object({
@@ -12,61 +9,45 @@ const RegisterForm = () => {
         repassword: Joi.valid(Joi.ref("password")),
     });
 
+    const [value, setValue] = useState({
+        username: "",
+        email: "",
+        password: "",
+        repassword: "",
+    });
+
+    const [previousInput, setPreviousInput] = useState("");
+
+    const [error, setError] = useState({
+        usernameError: "",
+        emailError: "",
+        passwordError: "",
+        repasswordError: "",
+    });
+
+    useEffect(() => {
+        const errorInput = previousInput + "Error";
+
+        const { error } = schema.validate({ [previousInput]: value[previousInput] });
+        const errorMessage = error ? error.details[0].message : "";
+
+        setError({ ...error, [errorInput]: errorMessage });
+    }, [value, previousInput]);
+
     const handleFormularChange = (e) => {
         e.preventDefault();
 
         const inputName = e.target.name;
         const inputValue = e.target.value;
 
-        const errorMsgSelector = document.querySelector(
-            'label[for="' + inputName + '"] .errorMessage'
-        );
-
-        const error = {};
-        const value = { [inputName]: inputValue };
-
-        if (inputName === "repassword") {
-            const passwordValue = document.querySelector("label[for=password] input").value;
-            value["password"] = passwordValue;
-        }
-
-        error[inputName] = schema.validate(value);
-        errorMsgSelector.innerHTML = error[inputName].error;
+        setPreviousInput(inputName);
+        setValue({ ...value, [inputName]: inputValue });
     };
 
     return (
-        <React.Fragment>
-            <form method='post' onChange={handleFormularChange}>
-                <label htmlFor='username'>
-                    <p>Username</p>
-                    <input type='text' name='username' />
-                    <p className='errorMessage'></p>
-                </label>
-                <br />
-                <br />
-                <label htmlFor='email'>
-                    <p>E-Mail</p>
-                    <input type='text' name='email' />
-                    <p className='errorMessage'></p>
-                </label>
-                <br />
-                <br />
-                <label htmlFor='password'>
-                    <p>Password</p>
-                    <input type='text' name='password' key='password' />
-                    <p className='errorMessage'></p>
-                </label>
-                <br />
-                <br />
-                <label htmlFor='repassword'>
-                    <p>Re-Password</p>
-                    <input type='text' name='repassword' />
-                    <p className='errorMessage'></p>
-                </label>
-
-                <input type='submit' />
-            </form>
-        </React.Fragment>
+        <>
+            <form method='post' id='test' onChange={handleFormularChange}></form>
+        </>
     );
 };
 
